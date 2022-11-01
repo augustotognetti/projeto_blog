28e04d06540ea92a9845ceac0ad63d7f18bf5f55
augustotognetti <105666841+augustotognetti@users.noreply.github.com>
2022-11-01 10:43:50 -0300

    js 2.0 - adicionado a funcao postagem

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,10 +1,36 @@
-//Cria uma variável para manipular um arquivo XMl
+//Cria uma variável para manipular um arquivo XML
 xmlhttp = new XMLHttpRequest();
 //Lê um determinado arquivo XML no diretório do projeto
-xmlhttp.open("GET", "xml/conteudo.xml", false);
+xmlhttp.open("GET","xml/conteudo.xml",false);
 //Envia o arquivo aberto para leitura
 xmlhttp.send();
 //Envia uma resposta do tipo XML
-xmldoc = xmlhttp.responseXML;
-//Cria um vetor a partir da quantidade de postagens
-x = xmldoc.getElementsByTagName("postagem");
+xmlDoc = xmlhttp.responseXML;
+//Cria um array a partir da quantidade de postagens
+x = xmlDoc.getElementsByTagName("postagem");
+
+function funcaoConteudo(){
+    for(i = x.length - 1;i > -1;i--){
+        document.write(
+        "<tr>" +
+            "<td>"+ x[i].getAttribute("codigo") +"</td>" +
+            "<td><img src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"' width='150'></td>" +
+            "<td><a href='postagem.html?codigo_postagem=" + i + "'>"+ x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</a></td>" +
+            "<td>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,150) +"...</td>" +
+        "</tr>");
+    }
+}
+
+function funcaoPostagem(){
+    //Armazenar a url completa
+    url = new URL(window.location.href);
+    parametro = url.searchParams;
+    i = parametro.get("codigo_postagem");
+    document.write(
+        "<tr>" +
+            "<td>"+ x[i].getAttribute("codigo") +"</td>" +
+            "<td><img src='imgs/"+ x[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"' width='150'></td>" +
+            "<td>"+ x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</td>" +
+            "<td>"+ x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue + "...</td>" +
+        "</tr>");
+}
